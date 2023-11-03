699fd9b34919860952b5e607418e246dd28003f8
benjas909 <benjamin.aguilerac@usm.cl>
2023-11-03 16:53:35 -0300

    añade prototipo de calculadora

--- frontend/src/views/sims/index.jsx
+++ frontend/src/views/sims/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function index() {
+    const calcularPrestamo = (event)=>{
+        
+    }
+
     return (
             <div>
-                TESTTESTTEST 
+                Simulación de prestamo
+                <form onSubmit={calcularPrestamo}>
+                    <label for="tasa">Tasa de prestamo:</label>
+                    <input type="text" id="tasa" name="tasa"></input><br></br>
+                    <label for="plazo">Plazo de prestamo:</label>
+                    <input type="text" id="plazo" name="plazo"></input><br></br>
+                    <input type="submit" value="Calcular"></input>
+                </form>
             </div>
+            
         )
 }
