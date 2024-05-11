ef0a597414a0176b304b63e921bbf8b0616fdfc4
pipeeeh1 <felipe14789632@gmail.com>
2024-05-10 23:53:48 -0400

    added tests

--- testing/test_del_test.py
+++ testing/test_del_test.py
@@ -1,56 +1,52 @@
+
 import requests
 import unittest
-from splinter import Browser
-import time
-
-def procesar_resultado(texto:str):
-    val = texto.split(":")[1].strip().split(" ")[0].strip()
-    return float(val)
-
-def simulacion(valorCredito, tasa, plazo):
-    tup = ['valorUF','cuotasUF','totalUF','cuotasCLP','totalCLP']
-    r = []
-    with Browser() as browser:
-        browser.visit("http://localhost:3000/")
-        browser.fill('valorCredito', valorCredito)
-        browser.fill('tasa', tasa)
-        browser.fill('plazo', plazo)
-        browser.find_by_id('botonPrestamo').first.click()
-        browser.find_by_id('botonPrestamo').first.click()
-        aux = lambda x: procesar_resultado(browser.find_by_id(x)[0].text)
-        r = list(map(aux,tup))
-    return r
-
-"""with Browser() as browser:
-    browser.visit("http://localhost:3000/")
-    browser.fill('valorCredito', '3')
-    browser.fill('tasa', '21')
-    browser.fill('plazo', '8')
-    browser.find_by_id('botonPrestamo').first.click()
-    browser.find_by_id('botonPrestamo').first.click()
-    copied_text = browser.find_by_id('valorUF')[0].text
-    copied_text2 = browser.find_by_id('cuotasUF')[0].text
-    copied_text3 = browser.find_by_id('totalUF')[0].text
-    copied_text4 = browser.find_by_id('cuotasCLP')[0].text
-    copied_text5 = browser.find_by_id('totalCLP')[0].text
-
-    print(procesar_resultado(copied_text))
-    print(procesar_resultado(copied_text2))
-    print(procesar_resultado(copied_text3))
-    print(procesar_resultado(copied_text4))
-    print(procesar_resultado(copied_text5))"""
-
-v = requests.get("https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=599cd22316598c0ec9fc843e23b2cdcc077159ba&formato=JSON")
-print(v.text)
+import json
 
 
 class LoanFormTest(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(cls):
-        cls.url = "http://localhost:3000/"
 
+    def test_create_user(self):
+        post = {"nombre":"usuario", "email":"email@email.cl","password":"contra"}
+        res = requests.post("http://localhost:8080/users/create", json=post)
+        print(" ------------ ")
+        print(" Inputs => nombre:usuario, email:email@email.cl,password:contra ")
+        print(" Salida esperada => codigo 200, creacion exitosa ")
+        print(" Contexto => Prueba de creacion usuario nuevo ")
+        print(" ------------ ")
+        print(res.status_code)
+        print(" ------------ ")
     
-    def basic_test(self):
-        valorUF,cuotasUF,totalUF,cuotasCLP,totalCLP = simulacion(1,1,1)
-        v = 
+        
+    def test_get_user(self):
+        res = json.loads(requests.get("http://localhost:8080/users").text)
+        print(" ------------ ")
+        print(" Inputs => ninguno")
+        print(" Salida esperada => retorna los usuarios de la base de datos ")
+        print(" Contexto => Prueba de GET tabla Users ")
+        print(" ------------ ")
+        for user in res:
+            print(user)
+
+    def test_get_clients(self):
+        res = json.loads(requests.get("http://localhost:8080/clients").text)
+        print(" ------------ ")
+        print(" Inputs => ninguno")
+        print(" Salida esperada => retorna los clientes de la base de datos ")
+        print(" Contexto => Prueba de GET tabla Clientes ")
+        print(" ------------ ")
+        for client in res:
+            print(client)
+    
+    def test_create_client(self):
+        post = {"id":11,"name":"usuario11", "age":"22"}
+        res = requests.post("http://localhost:8080/clients", json=post)
+        print(" ------------ ")
+        print(" Inputs => id:11, name:usuario11, age:22 ")
+        print(" Salida esperada => codigo 200, creacion exitosa ")
+        print(" Contexto => Prueba de creacion cliente nuevo ")
+        print(" ------------ ")
+        print(res.status_code)
+        print(" ------------ ")
+        
+unittest.main()
